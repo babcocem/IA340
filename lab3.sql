b5126e251fb0b080e8a939c42030132337dbc64f
babcocem <143033812+babcocem@users.noreply.github.com>
2023-09-18 14:27:47 -0400

    lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -6,19 +6,19 @@ values('s1@jmu.edu','s1','IA'),
   ('s2@jmu.edu','s2','IA'),
   ('s3@jmu.edu','s3','ISAT'),
   ('s4@jmu.edu','s4','ISAT');
-select * from student
+
   
 --2.2
 insert into professor(p_email,p_name,office)
 values('p1@jmu.edu','p1','o1'),
   ('p2@jmu.edu','p2','o2');
-select * from professor
+
 --2.3
 insert into course(c_number,c_name,room,p_email)
 values('c1','postgresql','r1','p1@jmu.edu'),
   ('c2','mongodb','r2','p2@jmu.edu'),
   ('c3','twitter','r1','p1@jmu.edu');
-select * from course
+
   
 --2.4
 insert into enroll(s_email,c_number)
@@ -28,7 +28,7 @@ values('s1@jmu.edu','c1'),
   ('s4@jmu.edu','c2'),
   ('s2@jmu.edu','c3'),
   ('s3@jmuedu','c3');
-select * from enroll_list
+
   
 --2.5
 insert into professor(p_email,p_name,office)
