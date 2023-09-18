50164c920d2dd4770bd5f0a1ae0f7d626af00c30
babcocem <143033812+babcocem@users.noreply.github.com>
2023-09-18 13:51:07 -0400

    lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -1,6 +1,14 @@
+--lab 3
+--query and modify data with sql
 --Q2.1
 insert into student(s_email,s_name,major)
-values('s1@jmu.edu','s1','IA');('s2@jmu.edu','s2','IA');('s3@jmu.edu','s3','ISAT');('s4@jmu.edu','s4','ISAT')
+values('s1@jmu.edu','s1','IA'),
+  ('s2@jmu.edu','s2','IA'),
+  ('s3@jmu.edu','s3','ISAT'),
+  ('s4@jmu.edu','s4','ISAT')
+;
+select * from student;
+  
 --Q2.2
 insert into professor(p_email,p_name,office)
 values('p1@jmu.edu','p1','o1');('p2@jmu.edu','p2','o2')
